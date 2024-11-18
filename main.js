435895b2db8f0caea50d9f8332811e0ad1717ebb
AADITYA SHANKAR <99283091+aadityaS18@users.noreply.github.com>
2024-11-18 23:22:37 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+
+const cursor = document.createElement('div');
+cursor.classList.add('custom-cursor');
+document.body.appendChild(cursor);
+
+document.addEventListener('mousemove', (e) => {
+    cursor.style.left = `${e.pageX}px`;
+    cursor.style.top = `${e.pageY}px`;
+});
